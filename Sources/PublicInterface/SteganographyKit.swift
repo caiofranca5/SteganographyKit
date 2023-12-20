e8810e0f23053b351817dfbf407abe5b90999631
Caio França <caiofranca5@hotmail.com>
2023-12-20 16:48:22 -0300

    fix: date format

--- Sources/PublicInterface/SteganographyKit.swift
+++ Sources/PublicInterface/SteganographyKit.swift
@@ -2,7 +2,7 @@
 //  SteganographyKit.swift
 //  SteganographyKit
 //
-//  Created by Caio Franca on 18 de dez. de 2023.
+//  Created by Caio França on 20/12/23.
 //  Copyright © 2023 SteganographyKit. All rights reserved.
 //
 
