830be57ba33528f9f29e9beca7126b9a19a630fa
Caio França <caiofranca5@hotmail.com>
2023-12-21 02:03:05 -0300

    feat: add support for objective-c

--- Sources/PublicInterface/SteganographyManager.swift
+++ Sources/PublicInterface/SteganographyManager.swift
@@ -17,14 +17,14 @@ public class SteganographyManager {
     ///   - message: The message to be encoded.
     ///   - image: The image in which the message will be encoded.
     /// - Returns: An image with the encoded message or nil if the operation fails.
-    public static func encodeMessage(_ message: String, inImage image: UIImage) -> UIImage? {
+    @objc public static func encodeMessage(_ message: String, inImage image: UIImage) -> UIImage? {
         return Steganographer.hideMessageInImage(image: image, message: message)
     }
 
     /// Decodes a message from an image.
     /// - Parameter image: The image containing the encoded message.
     /// - Returns: The decoded message or nil if no message is found or if the operation fails.
-    public static func decodeMessage(fromImage image: UIImage) -> String? {
+    @objc public static func decodeMessage(fromImage image: UIImage) -> String? {
         return Steganographer.readMessageFromImage(image: image)
     }
 
